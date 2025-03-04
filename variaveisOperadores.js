614e357e305aa0a9f54ac24c917cc986500d4b33
Pedro <85305216+Yaddz@users.noreply.github.com>
2025-03-04 13:40:29 -0300

    Create variaveisOperadores.js

---
+++ variaveisOperadores.js
@@ -0,0 +1,14 @@
+const numA = 4;
+const numB = 8;
+
+let result = numA + numB;
+console.log(`Resultado da soma entre ${numA} e ${numB}: ${result}`);
+
+result = numA - numB; 
+console.log(`Resultado da subtração entre ${numA} e ${numB}: ${result}`);
+
+result = numA * numB;
+console.log(`Resultado da multiplicação entre ${numA} e ${numB}: ${result}`);
+
+result = numA / numB;
+console.log(`Resultado da divisão entre ${numA} e ${numB}: ${result}`);
