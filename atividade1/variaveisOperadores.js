987d03b708fe331d801641c960527064bbd2f985
Pedro <85305216+Yaddz@users.noreply.github.com>
2025-03-18 12:56:28 +0000

    Add variaveisOperadores.js corrigida

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,31 @@
+let numA = 4;
+let numB = 8;
+
+const operacoes = ["Adição", "Subtração", "Multiplicação", "Divisão"]
+
+function imprimirResultado(operacao, operando1, operando2, resultado, ) {
+    console.log(`Resultado da ${operacao} entre ${numA} e ${numB}: ${resultado}`);
+}
+
+let resultado = numA + numB;
+imprimirResultado(operacoes[0], numA, numB, resultado);
+
+resultado = numA - numB;
+imprimirResultado(operacoes[1], numA, numB, resultado);
+
+resultado = numA * numB;
+imprimirResultado(operacoes[2], numA, numB, resultado);
+
+resultado = numA / numB;
+imprimirResultado(operacoes[3], numA, numB, resultado);
+
+/*
+function verificarParidade(n) {
+    if (n % 2 == o) {
+        return true;
+    }
+
+    return false;
+
+}
+*/
